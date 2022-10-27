22c6e4d57cb2e96320bb1621ba164687acd5f2a5
mstr <manuel.strenge@zuehlke.com>
2022-10-27 15:12:36 +0200

    Added Nvim base config and fixed tmux config

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,29 @@
+vim.opt.showmatch = true
+vim.opt.ignorecase = true
+vim.opt.mouse = "v" --middle mouse click paste
+vim.opt.mouse = "a" --enable mouse click
+vim.opt.hlsearch = true --hilight search
+vim.opt.incsearch = true -- incremental search
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.autoindent = true --indent newline the same amount as previous
+vim.opt.number = true --linenumber
+vim.opt.wildmode = "longest,list"
+vim.cmd [[
+  syntax enable
+  set nocompatible
+]]
+vim.opt.shell = "/bin/zsh"
+vim.opt.ttyfast = true
+vim.opt.clipboard = "unnamedplus"
+--vim.opt.guifont = "Iosevka Nerd Font Mono"
+vim.opt.ruler = true
+vim.opt.visualbell = true
+vim.opt.encoding = "utf-8"
+vim.opt.termguicolors = true
+vim.opt.autoread = true
+vim.opt.updatetime = 100
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+--vim.opt.nobackup = true
+--vim.opt.nowritebackup = true
